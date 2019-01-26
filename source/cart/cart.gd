c36ca40768e01dedea19f32373a1e26bf353ddfe
JohnMeadow <leszek.nowak@uj.edu.pl>
2019-01-26 12:28:40 +0100

    stuff

--- source/cart/cart.gd
+++ source/cart/cart.gd
@@ -4,20 +4,41 @@ export(float) var speed:float = 10.0
 
 var move:Vector2       = Vector2.ZERO
 
-var loot_in_cart = 10
+var bump_velcocity:float = 0.0
+var loot_in_cart:int = 10
+var target_stering:float = 0
+var timer:float = 0.0
+var current_stering:float = 0.0
+var stering_bias:float = 0.0
+
 
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	globals.wheel = $cart/wheel_position
+	globals.cart_node = self
 
 func _physics_process(delta):
 	move = Vector2.ZERO
-	move.x -= speed * delta
-	translate(move)
+	move.x -= speed 
+	move.y += current_stering * 3
+	translate(move.normalized() * speed * delta)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
+func _process(delta):
+	timer += delta * 10
+	stering_bias += rand_range(-(0.1+stering_bias*0.01), 0.1-stering_bias*0.01 )
+	target_stering += stering_bias
+	target_stering = clamp(target_stering, -10,10)
+
+	current_stering = lerp(current_stering, target_stering, 0.1)
+	$goat.position.y  = abs(sin(timer)*2)-40 + current_stering
+	$goat2.position.y = abs(cos(timer)*2)-27 + current_stering
+	$cart.position.y += bump_velcocity 
+	if $cart.position.y < -47:
+		bump_velcocity += 0.5
+	else:
+		$cart.position.y = -47
+		bump_velcocity = 0
 
 func _on_VisibilityNotifier_screen_exited():
 	print("wózek poza ekranem")
@@ -35,3 +56,7 @@ func _on_Area2D_body_entered(body):
 	
 func add_loot():
 	loot_in_cart += 1
+
+func _on_bump_area_entered(area):
+	area.get_node("Shape2D").disabled = true
+	bump_velcocity -= rand_range(1,3)
--- source/globals.gd
+++ source/globals.gd
@@ -1,20 +1,9 @@
 extends Node
 
-#const BLINDER_TIME = 3
 
-#var player_score       = [0,0,0,0]
-#var player_score_label = [null,null,null,null]
-#var player_label       = ["Red", "Green", "Violet", "Blue"]
-#var player_count       = 2
-#var players_enabled    = [true,false,false,false]
-#var victory_label      = "no"
-#var blinder            = null
-#var winnerLabel        = null
-#var won                = false
-#var timer              = 3
-#var screen             = Vector2()
-
+var wheel:Node = null
 var cart:Node2D = null
+var cart_node:Node = null
 var players_position:Node2D = null
 var camera:Camera2D = null
 
