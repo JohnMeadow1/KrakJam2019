282cd15906bd3dd2b334f67e417416beb88b49f6
John_Meadow <armitage3@gazeta.pl>
2019-01-27 01:32:34 +0100

    title added

--- source/main.gd
+++ source/main.gd
@@ -23,3 +23,4 @@ func handle_camera():
 	target_camera_position = $YSort/player_1.position + $YSort/player_2.position + $YSort/player_3.position + $YSort/player_4.position + $YSort/cart.position
 	target_camera_position *= 0.2
 	$Camera.position = lerp($Camera.position, target_camera_position, 0.1)
+	$Camera.position.x = min($Camera.position.x,-0 )
---
+++ source/title.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var timer = 0
+var start = false
+
+func _process(delta):
+	if start:
+		timer += delta
+		if timer > 2:
+			 get_tree().change_scene_to(load("res://main.tscn"))
+			
+	for i in range(4):
+		if Input.is_action_pressed("action_p"+str(i)):
+			start = true
+			$Sprite2.visible = true
+
